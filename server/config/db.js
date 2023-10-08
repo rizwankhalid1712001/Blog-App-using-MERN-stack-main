f08d9c0d9b220d3cbb13b9b9178f5221670e2f39
rizwankhalid <rizwankhalid696@gmail.com>
2023-10-08 13:07:09 +0500

    ll

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb+srv://rizwankhalid696:Rizwan%40123456@cluster0.luohxoi.mongodb.net/?retryWrites=true&w=majority").then(()=>{
-mongoose.connect("mongodb://0.0.0.0:27017/Rizwan_B").then(()=>{
+mongoose.connect("mongodb+srv://rizwankhalid696:Rizwan%40123456@cluster0.luohxoi.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+// mongoose.connect("mongodb://0.0.0.0:27017/Rizwan_B").then(()=>{
     console.log("connected!");
 }).catch((err)=>{
     console.log("error", err);
