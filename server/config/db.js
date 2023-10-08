7da00060510e39ace42c899e4a8843b4865fc56d
rizwankhalid <rizwankhalid696@gmail.com>
2023-10-08 13:08:59 +0500

    kk

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,5 @@ mongoose.connect("mongodb+srv://rizwankhalid696:Rizwan%40123456@cluster0.luohxoi
 // mongoose.connect("mongodb://0.0.0.0:27017/Rizwan_B").then(()=>{
     console.log("connected!");
 }).catch((err)=>{
-    console.log("error", err);
+    console.log("errorkggfw", err);
 })
